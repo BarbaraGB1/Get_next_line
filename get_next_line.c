be957f6be9eb5b5d717905b0679b8ac984251e79
BarbaraGB1 <bargarci@c1r10s1.42madrid.com>
2023-05-07 13:12:39 +0200

    a

--- get_next_line.c
+++ get_next_line.c
@@ -6,10 +6,148 @@
 /*   By: bargarci <bargarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:14:30 by bargarci          #+#    #+#             */
-/*   Updated: 2023/04/29 17:15:07 by bargarci         ###   ########.fr       */
+/*   Updated: 2023/05/05 16:36:42 by bargarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-char *get_next_line(int fd)
+/*# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>*/
+#include "get_next_line.h"
+int		count(char *buffer)
 {
+	int	i;
+	
+	i = 0;
+	if (!buffer)
+		return (0);
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+
 }
+int	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+char	*ft_new_line(char *s)
+{	
+	char *new;
+	int	i;
+
+	i = 0;
+	if (!s)
+		return(0);
+	while (s[i] != '\n' && s[i] != '\0')
+		i++;
+	if (s[i] == '\n')
+		new = malloc(sizeof(char) *(i + 2));
+	else 
+		new = malloc(sizeof(char) *(i + 1));
+	if(!new)
+		return (0);
+	i = 0;
+	while (s[i] != '\n' && s[i] != '\0')
+	{
+		new[i] = s[i];
+		i++;
+	}
+	if (new[i] == '\n')
+		new[i++] = '\n';
+	new[i] = '\0';
+	return (new);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (0);
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (0);
+	while (s1[i] != '\0')
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	free (s1);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*rest;
+	char		*buffer;
+	int			nbyts;
+	char 	*new_line;
+
+	nbyts = 1;
+	if (fd < 0 || BUFFER_SIZE < 0 || fd > OPEN_MAX)
+		return (NULL);
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (0);
+	while (!count(rest) && nbyts != 0)
+	{
+		nbyts = read(fd, buffer, BUFFER_SIZE);
+		if (nbyts == -1)
+		{
+			free (buffer);
+			return (0);
+		}
+		buffer[nbyts] = '\0';
+		rest = ft_strjoin(rest, buffer);
+		new_line = ft_new_line(rest);
+	}
+	return (new_line);
+	
+}
+
+int main(void)
+{
+	int fd;
+	char *line;
+
+	fd = open("read.txt", O_RDONLY);
+	if (fd < 0)
+		return (0);
+	while ((line = get_next_line(fd)))
+	{
+		printf("%s", line);
+		free (line);
+
+	}
+	close(fd);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,26 @@
 /*   By: bargarci <bargarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:15:59 by bargarci          #+#    #+#             */
-/*   Updated: 2023/04/29 17:17:37 by bargarci         ###   ########.fr       */
+/*   Updated: 2023/05/04 18:19:28 by bargarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+#ifndef BUFFER_SIZE 
+# define BUFFER_SIZE 5
+#endif
+char	*get_next_line(int fd);
+int		new_line(char *buffer);
+char	*ft_strjoin(char *s1, char *s2);
+int	ft_strlen(const char *s);
+char	*ft_new_line(char *s);
+#endif
